6ed44edf7ca6234ba975490637760bc89787ff3f
Matheus Tucunduva Pinheiro <mtucunduva98@gmail.com>
2020-05-18 17:28:09 -0300

    Update nmapbuster.sh

--- nmapbuster.sh
+++ nmapbuster.sh
@@ -16,12 +16,12 @@ nmapBuster(){
 
         nmap -vv -p- -oA $1/full_port_scan $1
 
+        echo ""
+        echo "Initial scan done"
 
         PORTS=$(cat $1/full_port_scan.gnmap | grep -oE '[0-9]{2,5}[/](open|closed|filtered)' | cut -d "/" -f1 | sed -z 's/\n/,/g')
         PORTS_FILTERED=${PORTS::-1}
         
-        echo ""
-        echo "Initial scan done"
         echo ""
         echo "Command:"
         echo "nmap -vv -A -sC -sV -oA $1/script_version_scan $1 -p $PORTS_FILTERED"
